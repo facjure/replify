4a09b005ae30326e774a29295e976cbb9de6ab21
Priyatam Mudivarti <priyatam@gmail.com>
2016-01-16 18:50:14 -0800

    up deps
    include new cljs, tools repl, and leiningen

--- project.clj
+++ project.clj
@@ -1,21 +1,21 @@
-(defproject priyatam/replify "0.2.4"
-  :description "A minimalist Clojurescript repl and build system"
-  :url "https://github.com/priyatam/replify"
-  :scm {:name "git" :url "https://github.com/priyatam/replify"}
+(defproject facjure/replify "0.3.0"
+  :description "An minimalist Clojurescript repl and build tool"
+  :url "https://github.com/facjure/replify"
+  :scm {:name "git" :url "https://github.com/facjure/replify"}
   :license {:name "Eclipse Public License" :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :jvm-opts ^:replace ["-Xms512m" "-XX:+UseConcMarkSweepGC" "-XX:+CMSParallelRemarkEnabled" "-server"]
+  :jvm-opts ^:replace ["-Xmx128m" "-server"]
   :dependencies [[org.clojure/clojure "1.7.0" :scope "provided"]
-                 [org.clojure/clojurescript "1.7.28" :scope "provided"]
-                 [org.clojure/tools.cli "0.3.1"]
+                 [org.clojure/clojurescript "1.7.228" :scope "provided"]
+                 [org.clojure/tools.cli "0.3.2"]
+                 [org.clojure/tools.nrepl "0.2.12"]
                  [me.raynes/fs "1.4.6"]
+                 [leiningen "2.5.3"]
                  [alembic "0.3.2"]
-                 [garden "1.2.5"]]
-  :npm {:dependencies [[source-map-support "0.3.1"]]}
+                 [garden "1.3.0"]]
+  :npm {:dependencies [[source-map-support "0.4.0"]]}
   :plugins [[lein-npm "0.6.1"]
-            [lein-garden "0.2.5"]]
+            [lein-garden "0.2.6"]]
   :source-paths ["src" "target/classes"]
   :clean-targets ["target" "dist"]
   :target-path "target"
-  :profiles {:dev {:dependencies [[org.clojure/tools.nrepl "0.2.10"]
-                                  [leiningen "2.5.1"]]}}
   :uberjar-name "replify.jar")
