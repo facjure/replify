91a5efc2f8c8a0f80e3ea1b095a09c64433c213e
Priyatam Mudivarti <priyatam@gmail.com>
2017-10-12 21:30:07 -0700

    remove figwheel

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
  (defproject facjure/replify "0.4.0-SNAPSHOT"
-  :description "A minimalist Clojurescript repl with embedded build tools"
+  :description "A minimalist Clojurescript repl and minimalist build tool"
   :url "https://github.com/facjure/replify"
   :scm {:name "git" :url "https://github.com/facjure/replify"}
   :license {:name "Eclipse Public License" :url "http://www.eclipse.org/legal/epl-v10.html"}
@@ -11,14 +11,10 @@
                  [org.clojure/clojurescript "1.9.946"]
                  [org.clojure/core.async "0.3.443"]
                  [org.clojure/tools.cli "0.3.5"]
-                 [org.clojure/tools.nrepl "0.2.13"]
                  [me.raynes/fs "1.4.6"]
                  [alembic "0.3.2"]
-                 [figwheel "0.5.14"]
-                 [figwheel-sidecar "0.5.14"]
                  [garden "1.3.3"]
                  [leiningen "2.8.0-RC1"]
-                 [lein-figwheel "0.5.14"]
                  [lein-garden "0.2.8"]]
   :npm {:dependencies [[source-map-support "0.5.0"]]}
   :plugins [[lein-doo "0.1.8"]
